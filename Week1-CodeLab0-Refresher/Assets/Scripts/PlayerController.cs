d67ed543cb3be4493fd9d3cd725944ab39d32246
scuddamn <aescudder@gmail.com>
2020-02-03 15:45:06 -0500

    Classwork (created basic gameplay with arena, prize, and one player) + made code object oriented + added player two

---
+++ Week1-CodeLab0-Refresher/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float force = 5;
+    Rigidbody2D rb;
+    public KeyCode up;
+    public KeyCode down;
+    public KeyCode right;
+    public KeyCode left;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello, World!");
+
+        rb = GetComponent<Rigidbody2D>();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //RIGHT
+        //if <right> is pressed
+       if (Input.GetKey(right)){
+            //apply a force using the "force" var
+            rb.AddForce(Vector2.right * force);
+        }
+
+       //LEFT
+        //if <left> is pressed
+        if (Input.GetKey(left)) {
+            //apply a force using the "force" var
+            rb.AddForce(Vector2.left * force);
+        }
+
+        //DOWN
+        //if <down> is pressed
+        if (Input.GetKey(down)) {
+            //apply a force using the "force" var
+            rb.AddForce(Vector2.down * force);
+        }
+
+        //UP
+        //if <up> is pressed
+        if (Input.GetKey(up)){
+            //apply a force using the "force" var
+            rb.AddForce(Vector2.up * force);
+        }
+
+    }
+}
